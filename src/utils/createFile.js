711ac99e96f5d11a4a9f25e969e02fe0c3378494
Zach Kusz <z.s.kusz@gmail.com>
2024-04-19 18:54:04 -0500

    format dates and add blog content placeholder

--- src/utils/createFile.js
+++ src/utils/createFile.js
@@ -1,3 +1,17 @@
+// date as MM-DD-YYYY and in strings
+const formatDate = (date) => {
+    if (!date) date = new Date();
+
+    const dateObj = new Date(date);
+    const year = dateObj.getFullYear();
+    let month = (1 + dateObj.getMonth()).toString();
+    month = month.length > 1 ? month : '0' + month;
+    let day = dateObj.getDate().toString();
+    day = day.length > 1 ? day : '0' + day;
+
+    return `'${month}-${day}-${year}'`;
+};
+
 const getHeroIndex = (photos) => {
     let heroPhotoIndex = photos.findIndex((photo) => photo.useAsHeroPhoto);
     const builtImageIndex = photos.findIndex((photo) => photo.isFinalBuildPhoto);
@@ -23,7 +37,9 @@ export default function createFile(formData, photos) {
     formData.entries().forEach(([label, value]) => {
         if (label === 'publishDate') {
             blobParts.push(`dates:\n`);
-            blobParts.push(`  - ${value}\n`);
+            blobParts.push(`  - ${formatDate(value)}\n`);
+        } else if (label.toLowerCase().includes('date')) {
+            blobParts.push(`${label}: ${formatDate(value)}\n`);
         } else if (!label.includes('photo')) {
             blobParts.push(`${label}: ${value}\n`);
         }
@@ -53,6 +69,8 @@ export default function createFile(formData, photos) {
     });
 
     blobParts.push('---\n');
+    blobParts.push('\n');
+    blobParts.push("<!-- Bob Law's Law Blog Content Goes Here! -->\n");
 
     return new Blob(blobParts, blobOptions);
 }
